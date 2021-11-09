364ad703a7bdecc5e3e58edeed35ba573e18301a
EyaZidi19 <3805@holbertonschool.com>
2021-11-09 13:05:21 -0800

    print_string

---
+++ print_string.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * print_character - print a character
+ * @arg: argument of type va_list
+ * Return: always 1
+ */
+int print_character(va_list arg)
+{
+	char ch;
+
+	ch = va_arg(arg, int);
+	_putchar(ch);
+	return (1);
+}
+/**
+ *print_string - print a string
+ *@arg: argument of type of va_list
+ * Return: len
+ */
+int print_string(va_list arg)
+{
+	int len = 0;
+	char *str;
+
+	str = va_arg(arg, char*);
+	if (str = NULL)
+		str = "(null)";
+	while (str[len] != '\0')
+	{
+		_putchar(str[len]);
+		len++;
+	}
+	return (len);
+}
+/**
+ *print_percent - print percent
+ *@arg: argument of type of va_list
+ * Return: integer
+ */
+int print_percent(va_list arg)
+{
+	(void)arg;
+
+	_putchar('%');
+	return (0);
+}
