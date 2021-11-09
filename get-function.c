a2e5b6189b0e33e5bae0fc22456627bb58ee8940
EyaZidi19 <3805@holbertonschool.com>
2021-11-09 12:41:16 -0800

    get-function

--- get-function.c
+++ get-function.c
@@ -7,7 +7,7 @@
  */
 int check_char_func(const char s, va_list ap)
 {
-	int i = 0, count == 0;
+	int i = 0, count = 0;
 	printf_t ch[] = {
 		{'c', print_character},
 		{'s', print_string},
@@ -19,9 +19,9 @@ int check_char_func(const char s, va_list ap)
 		{'R', print_rot13},
 		{0, NULL}
 	};
-	while (ch[i]c != 0)
+	while (ch[i].c != 0)
 	{
-		if (ch[i].c  = s)
+		if (ch[i].c == s)
 		{
 			count = count + ch[i].fun(ap);
 			return (count);
