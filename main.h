3d37b321bbc177e6928e7c98f17e905a9ee8b840
EyaZidi19 <3805@holbertonschool.com>
2021-11-10 03:43:02 -0800

    header file

--- main.h
+++ main.h
@@ -1,5 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef 
+#define
+
 #include <unistd.h>
 #include <stdio.h>
 #include <stdarg.h>
@@ -14,11 +15,11 @@ typedef struct printf
 	char c;
 	int (*fun)(va_list);
 } printf_t;
-/* functions prototype */
+
 int check_char_func(const char s, va_list ap);
 int _printf(const char *format, ...);
 int _putchar(char c);
-/* functions prototypes conversion */
+
 int print_character(va_list arg);
 int print_percent(va_list arg);
 int print_string(va_list arg);
@@ -27,4 +28,6 @@ int print_rev_string(va_list arg);
 int print_rot13(va_list arg);
 int print_binary(va_list arg);
 int binary(unsigned int n);
-#endif /* MAIN_H*/
+
+
+#endif 
