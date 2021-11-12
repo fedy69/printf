adf1df0bbc76f643f057891ecfe80484c01b2265
Fedy chihaoui <84166701+fedy69@users.noreply.github.com>
2021-11-12 09:27:28 +0100

    function.c

--- function.c
+++ function.c
@@ -1,23 +1,21 @@
 #include "main.h"
 /**
- * check_char_func - check depending on separator
+ * pick_function - check depending on separator
  * @s: char
  *@ap: argument of type va_list
  * Return: int
  */
-int check_char_func(const char s, va_list ap)
+int pick_function(const char s, va_list ap)
 {
 	int i = 0, count = 0;
-	printf_t ch[] = {
-		{'c', print_character},
-		{'s', print_string},
-		{'%', print_percent},
-		{'d', print_integer},
-		{'i', print_integer},
-		{'b', print_binary},
-		{'r', print_rev_string},
-		{'R', print_rot13},
-		{0, NULL}
+	arr_of_p ch[] = {
+		{'c', p_char},
+		{'s', p_string},
+		{'%', p_percent},
+		{'d', p_int},
+		{'i', p_int},
+		{'b', p_binary},
+
 	};
 	while (ch[i].c != 0)
 	{
