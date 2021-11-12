0fae5ac18206d1118fc21d9a43ec161ba70c8820
Fedy chihaoui <84166701+fedy69@users.noreply.github.com>
2021-11-12 09:30:04 +0100

    main.h

--- main.h
+++ main.h
@@ -1,12 +1,14 @@
-#ifndef MAIN_H 
-#define  MAIN_H
+#ifndef MAINH
+#define MAINH
 
+#include <limits.h>
 #include <unistd.h>
-#include <stdio.h>
 #include <stdarg.h>
-#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 /**
- * struct printf - struct printf
+ * struct printf - struct for printf
  * @c: The character
  * @fun: The function associated
  */
@@ -14,17 +16,15 @@ typedef struct printf
 {
 	char c;
 	int (*fun)(va_list);
-} printf_t;
+} arr_of_p;
 
-int check_char_func(const char s, va_list ap);
-int _printf(const char *format, ...);
+int p_char(va_list arg);
+int p_string(va_list arg);
+int p_percent(va_list arg);
+int p_int(va_list arg);
 int _putchar(char c);
+int pick_function(const char s, va_list ap);
+int _printf(const char *format, ...);
+int p_binary(va_list arg);
 
-int print_character(va_list arg);
-int print_string(va_list arg);
-int print_integer(va_list arg);
-int print_rev_string(va_list arg);
-int print_rot13(va_list arg);
-
-
-#endif 
+#endif
